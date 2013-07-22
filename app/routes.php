7c965b467c3e1d2e3a9361265bd265272a42955a
Richard Stoutner <richard@mashica.org>
2013-07-22 16:37:59 -0500

    removed files from public. Now using view.

--- app/routes.php
+++ app/routes.php
@@ -13,5 +13,44 @@
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	//return View::make('hello');
+	return View::make('mashica');
 });
+
+/*
+Route::get('users', function()
+{
+    return View::make('users');
+});
+*/
+
+
+
+
+Route::get('users', function()
+{
+    $users = User::all();
+
+    return View::make('users')->with('users', $users);
+});
+
+
+/*
+Route::get('mashica', function()
+{
+	//return View::make('hello');
+	return View::make('mashica2');
+	//return $name;
+
+});
+*/
+
+/*
+Route::get('mashica/{name?}', function($name = null)
+{
+	//return View::make('hello');
+	return View::make('mashica2');
+	//return $name;
+
+});
+*/
